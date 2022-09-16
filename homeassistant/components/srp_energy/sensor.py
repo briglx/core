18f7ddfdde99e557bb24e1ceee10a22abbe6163d
Brig Lamoreaux <bklamoreaux@gmail.com>
2022-09-16 16:57:41 +0000

    Update config flow.

--- homeassistant/components/srp_energy/sensor.py
+++ homeassistant/components/srp_energy/sensor.py
@@ -1,6 +1,5 @@
 """Support for SRP Energy Sensor."""
 from datetime import datetime, timedelta
-import logging
 
 import async_timeout
 from requests.exceptions import ConnectionError as ConnectError, HTTPError, Timeout
@@ -14,19 +13,20 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ATTRIBUTION, ENERGY_KILO_WATT_HOUR
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import StateType
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .const import (
     ATTRIBUTION,
+    CONF_IS_TOU,
     DEFAULT_NAME,
+    DOMAIN,
     ICON,
+    LOGGER,
     MIN_TIME_BETWEEN_UPDATES,
     SENSOR_NAME,
     SENSOR_TYPE,
-    SRP_ENERGY_DOMAIN,
 )
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -34,10 +34,10 @@ async def async_setup_entry(
 ) -> None:
     """Set up the SRP Energy Usage sensor."""
     # API object stored here by __init__.py
-    is_time_of_use = False
-    api = hass.data[SRP_ENERGY_DOMAIN]
-    if entry and entry.data:
-        is_time_of_use = entry.data["is_tou"]
+    is_time_of_use: bool = getattr(entry, CONF_IS_TOU, False)
+    api = hass.data[DOMAIN][entry.entry_id]
+
+    LOGGER.debug("sensor: async_setup_entry apti: %s %s", type(api), api)
 
     async def async_update_data():
         """Fetch data from API endpoint.
@@ -45,10 +45,12 @@ async def async_setup_entry(
         This is the place to pre-process the data to lookup tables
         so entities can quickly look up their data.
         """
+        LOGGER.debug("sensor: async_update_data enter")
         try:
             # Fetch srp_energy data
             start_date = datetime.now() + timedelta(days=-1)
             end_date = datetime.now()
+
             async with async_timeout.timeout(10):
                 hourly_usage = await hass.async_add_executor_job(
                     api.usage,
@@ -57,9 +59,22 @@ async def async_setup_entry(
                     is_time_of_use,
                 )
 
+                LOGGER.debug(
+                    "sensor: async_update_data: Received %s records from %s to %s",
+                    len(hourly_usage) if hourly_usage else "None",
+                    start_date,
+                    end_date,
+                )
+
                 previous_daily_usage = 0.0
                 for _, _, _, kwh, _ in hourly_usage:
                     previous_daily_usage += float(kwh)
+
+                LOGGER.debug(
+                    "sensor: async_update_data: previous_daily_usage %s",
+                    previous_daily_usage,
+                )
+
                 return previous_daily_usage
         except (TimeoutError) as timeout_err:
             raise UpdateFailed("Timeout communicating with API") from timeout_err
@@ -68,7 +83,7 @@ async def async_setup_entry(
 
     coordinator = DataUpdateCoordinator(
         hass,
-        _LOGGER,
+        LOGGER,
         name="sensor",
         update_method=async_update_data,
         update_interval=MIN_TIME_BETWEEN_UPDATES,
@@ -94,44 +109,43 @@ class SrpEntity(SensorEntity):
         self._state = None
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the sensor."""
         return f"{DEFAULT_NAME} {self._name}"
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         """Return sensor unique_id."""
         return self.type
 
     @property
-    def native_value(self):
+    def native_value(self) -> StateType:
         """Return the state of the device."""
         if self._state:
             return f"{self._state:.2f}"
         return None
 
     @property
-    def native_unit_of_measurement(self):
+    def native_unit_of_measurement(self) -> str:
         """Return the unit of measurement of this entity, if any."""
         return self._unit_of_measurement
 
     @property
-    def icon(self):
+    def icon(self) -> str:
         """Return icon."""
         return ICON
 
     @property
-    def usage(self):
+    def usage(self) -> str:
         """Return entity state."""
+        usage: float = 0.0
         if self.coordinator.data:
-            return f"{self.coordinator.data:.2f}"
-        return None
+            usage = self.coordinator.data
+        return f"{usage:.2f}"
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, str]:
         """Return the state attributes."""
-        if not self.coordinator.data:
-            return None
         attributes = {
             ATTR_ATTRIBUTION: ATTRIBUTION,
         }
@@ -139,17 +153,17 @@ class SrpEntity(SensorEntity):
         return attributes
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Return if entity is available."""
         return self.coordinator.last_update_success
 
     @property
-    def device_class(self):
+    def device_class(self) -> str:
         """Return the device class."""
         return SensorDeviceClass.ENERGY
 
     @property
-    def state_class(self):
+    def state_class(self) -> str:
         """Return the state class."""
         return SensorStateClass.TOTAL_INCREASING
 
--- tests/components/srp_energy/test_sensor.py
+++ tests/components/srp_energy/test_sensor.py
@@ -2,13 +2,13 @@
 from unittest.mock import MagicMock
 
 from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
-from homeassistant.components.srp_energy.const import (
+from homeassistant.components.srp_energy import (
     ATTRIBUTION,
     DEFAULT_NAME,
+    DOMAIN,
     ICON,
     SENSOR_NAME,
     SENSOR_TYPE,
-    SRP_ENERGY_DOMAIN,
 )
 from homeassistant.components.srp_energy.sensor import SrpEntity, async_setup_entry
 from homeassistant.const import ATTR_ATTRIBUTION, ENERGY_KILO_WATT_HOUR
@@ -28,7 +28,8 @@ async def test_async_setup_entry(hass):
             "password": "mypassword",
         }
     )
-    hass.data[SRP_ENERGY_DOMAIN] = fake_srp_energy_client
+
+    hass.data[DOMAIN] = fake_srp_energy_client
 
     await async_setup_entry(hass, fake_config, fake_async_add_entities)
 
@@ -39,18 +40,23 @@ async def test_async_setup_entry_timeout_error(hass):
     fake_srp_energy_client = MagicMock()
     fake_srp_energy_client.usage.return_value = [{1, 2, 3, 1.999, 4}]
     fake_config = MagicMock(
+        entry_id="testid",
         data={
             "name": "SRP Energy",
             "is_tou": False,
             "id": "0123456789",
             "username": "testuser@example.com",
             "password": "mypassword",
-        }
+        },
     )
-    hass.data[SRP_ENERGY_DOMAIN] = fake_srp_energy_client
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][fake_config.entry_id] = fake_srp_energy_client
     fake_srp_energy_client.usage.side_effect = TimeoutError()
 
     await async_setup_entry(hass, fake_config, fake_async_add_entities)
+
+    assert fake_srp_energy_client.usage.call_count == 1
+
     assert not fake_async_add_entities.call_args[0][0][
         0
     ].coordinator.last_update_success
@@ -70,7 +76,8 @@ async def test_async_setup_entry_connect_error(hass):
             "password": "mypassword",
         }
     )
-    hass.data[SRP_ENERGY_DOMAIN] = fake_srp_energy_client
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][fake_config.entry_id] = fake_srp_energy_client
     fake_srp_energy_client.usage.side_effect = ValueError()
 
     await async_setup_entry(hass, fake_config, fake_async_add_entities)
@@ -104,21 +111,13 @@ async def test_srp_entity(hass):
     assert not fake_coordinator.async_add_listener.data.called
 
 
-async def test_srp_entity_no_data(hass):
-    """Test the SrpEntity."""
-    fake_coordinator = MagicMock(data=False)
-    srp_entity = SrpEntity(fake_coordinator)
-    srp_entity.hass = hass
-    assert srp_entity.extra_state_attributes is None
-
-
 async def test_srp_entity_no_coord_data(hass):
     """Test the SrpEntity."""
     fake_coordinator = MagicMock(data=False)
     srp_entity = SrpEntity(fake_coordinator)
     srp_entity.hass = hass
 
-    assert srp_entity.usage is None
+    assert srp_entity.usage == "0.00"
 
 
 async def test_srp_entity_async_update(hass):
